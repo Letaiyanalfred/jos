3c51274d45db94264f03684659c52ac485afb8ff
letaiyanalfred <88006541+Letaiyanalfred@users.noreply.github.com>
2022-07-02 15:56:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Examination Grading and Management System</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link rel="shortcut icon" href="assets/img/ronk1.jpg" />
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet"> -->
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style22.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <a class="navbar-brand" href="index.php"></a>
+      <h1 class="logo mr-auto"><a href="index.php">Examination Grading and Management System</a></h1>
+     
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          
+          <li class="drop-down"><a href="">Login <i class='bx bxs-down-arrow'></i></a> 
+            <ul>
+              <li><a href="adminLogin.php">Admin</a></li>
+              <li><a href="studentLogin.php">Student</a></li>
+			  <!-- Log on to codeastro.com for more projects! -->
+             
+            </ul>
+          </li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header>
+
+ 
+  <section id="hero" class="d-flex justify-content-center align-items-center" style="background-image: url('image1.PNG');">
+    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Examination Grading and Mnagement System</h1>
+   
+    </div>
+  </section>
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Our Mission</h2>
+          <p>Our Mission</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+           
+          </div>
+         
+            <p>
+Inventing the  Future of Kenyan citizens by empowering them  with formally acquired knowledge
+</p>
+        </div>
+
+      </div>
+    </section>
+
+    
+    <section id="counts" class="counts section-bg">
+      <div class="container">
+
+        <div class="row counters">
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">5000</span>
+            <p>Students</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">9</span>
+            <p>Departments</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">12</span>
+            <p>Events</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">5</span>
+            <p>Faculties</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Examination Grading and management System </h3>
+            <p>
+              Moi Avenue,<br>
+              Nairobi,Kenya<br><br>
+              <strong>Phone:</strong>0112730530<br>
+              <strong>Email:</strong>mkandoojosephine72@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4> Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4> Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Service 1</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Service 2</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Service 3</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Service 4</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Service 5</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4> Newsletter</h4>
+            <p> For now, there are no latest news!</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" style="background-color:black ;" value="Subscribe Now!!">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; <strong><span>Examination Grading and Management System</span></strong> - <?php echo date('Y');?> -Developed by Josephine Mkandoo
+        <div class="credits">
+          
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
